306957ae71456e2d89c2dfe2a5b056c1baf89314
Lena <83307034+LenaAtReplit@users.noreply.github.com>
2024-01-23 15:45:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import os
 import asana
 
-# replace with your personal access token.
+# In Secrets, replace ASANA_PAT with your personal access token.
 
 # Check and set our environment variables
 personal_access_token = None
 if 'ASANA_PAT' in os.environ:
     personal_access_token = os.environ['ASANA_PAT']
 else:
-    print "No value for PAT in your console environment"
-    print "Try running this as `ASANA_PAT=0/1234567890abcdef... python hello_world.py`"
+    print("No value for PAT in your console environment")
+    print ("Try running this as `ASANA_PAT=0/1234567890abcdef... python hello_world.py`")
     quit()
 
 client = asana.Client.access_token(personal_access_token)
 
 me = client.users.me()
 
-print "Hello world! " + "My name is " + me['name'] + "."
-
+print("Hello world! " + "My name is " + me['name'] + ".")
